b078536e068303939edb092a074301ea3e26bb55
aamir <wasi3aamir@gmail.com>
2019-01-19 15:17:46 +0600

    basicSyntax

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxInt = int.MaxValue;
+            int minInt = int.MinValue;
+            float maxFloat = int.MaxValue;
+            float minFloat = int.MinValue;
+
+            Console.WriteLine("Max int :"+maxInt);
+            Console.WriteLine("Min int :"+minInt);
+            Console.WriteLine("Max Float :"+maxFloat);
+            Console.WriteLine("Min float :"+minFloat);
+        }
+    }
+}
